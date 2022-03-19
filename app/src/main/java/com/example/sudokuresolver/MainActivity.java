68644a8ea36c9a28e49eed66e692c7c02f1d08f0
PrzemoNo1 <37351230+PrzemoNo1@users.noreply.github.com>
2022-03-19 13:46:51 +0100

    Create initial GUI

--- app/src/main/java/com/example/sudokuresolver/MainActivity.java
+++ app/src/main/java/com/example/sudokuresolver/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.sudokuresolver;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onConfirmClick(View view) {
+        EditText numberOneOne = findViewById(R.id.OneOne);
+
+        TextView txtView = findViewById(R.id.Test12);
+        txtView.setText(numberOneOne.getText());
     }
 }
